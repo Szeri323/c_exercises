d21d7641b4f4fbe92d55cb03758ecf0d8ccea71e
Szeri <szeri323@gmail.com>
2025-02-09 20:41:42 +0100

    add histogram printing

--- c_book/ad1-13-horizontal-vertical-histograms.c
+++ c_book/ad1-13-horizontal-vertical-histograms.c
@@ -3,7 +3,7 @@
 #define IN 1  /* Inside the word */
 #define OUT 0 /* Outsie the word */
 
-void horizontal_histogram(int char_counter)
+void vertical_histogram(int char_counter)
 {
     for (int i = 1; i < char_counter; ++i)
     {
@@ -12,19 +12,20 @@ void horizontal_histogram(int char_counter)
     printf("(%d)", char_counter - 1);
 }
 
-void vertical_histogram(int word_lengths[10], int index, int max)
+void horizontal_histogram(int word_lengths[10], int last_used_index, int max_number_of_chars)
 {
-    /* Prints vertical histogram (to invert the axis switch max loop to ascending) */
-    for (int i = max; i >= 0; --i)
+    /* Prints horizontal histogram (to invert the axis switch max_number_of_chars loop to ascending) */
+    for (int i = max_number_of_chars; i >= 0; --i)
     {
-        for (int j = 0; j < index; ++j)
+        for (int j = 0; j < last_used_index; ++j)
         {
-            // printf("wl: %d max: %d", word_lengths[j] - 1, max);
+            // printf("wl: %d max_number_of_chars: %d", word_lengths[j] - 1, max_number_of_chars);
             if (word_lengths[j] - 1 > i)
             {
                 putchar('|');
             }
-            else {
+            else
+            {
                 putchar(' ');
             }
         }
@@ -34,26 +35,26 @@ void vertical_histogram(int word_lengths[10], int index, int max)
 
 int main()
 {
-    int c, char_counter, index, max;
+    int c, char_counter, last_used_index, max_number_of_chars;
     int word_lengths[10];
-    char_counter = index = max = 0;
+    char_counter = last_used_index = max_number_of_chars = 0;
     while ((c = getchar()) != EOF)
     {
         ++char_counter;
         if (c == ' ' || c == '\n' || c == '\t')
         {
             putchar('\n');
-            horizontal_histogram(char_counter);
-            if (char_counter > max)
+            vertical_histogram(char_counter);
+            if (char_counter > max_number_of_chars)
             {
-                max = char_counter;
+                max_number_of_chars = char_counter;
             }
-            word_lengths[index] = char_counter;
-            ++index;
+            word_lengths[last_used_index] = char_counter;
+            ++last_used_index;
             char_counter = 0;
         }
     }
     putchar('\n');
-    vertical_histogram(word_lengths, index, max - 1);
+    horizontal_histogram(word_lengths, last_used_index, max_number_of_chars - 1);
     putchar('\n');
 }
--- c_book/ad1-14.c
+++ c_book/ad1-14.c
@@ -1,21 +1,48 @@
 #include <stdio.h>
 
-void print_histogram()
+#define SIZE_OF_ASCII_TABLE 127
+
+void print_histogram(int word_lengths[127], int last_used_index, int max)
 {
+    // Vertical size of histogram
+    for (int i = max; i >= 0; --i)
+    {
+        // fix for all characters in ascii
+        for (int j = 97; j < 123; ++j)
+        {
+            if (word_lengths[j] >= i && i != 0)
+            {
+                putchar('|');
+            }
+            else if (i == 0)
+            {
+                printf("%c", j);
+            }
+            else
+            {
+                putchar(' ');
+            }
+        }
+        putchar('\n');
+    }
 }
 
 int main()
 {
 
-    int c;
+    int c, max;
 
-    int digits[127];
+    max = 0;
 
+    int digits[SIZE_OF_ASCII_TABLE];
+
+    // Table initialization
     for (int i = 0; i < 128; ++i)
     {
         digits[i] = 0;
     }
 
+    // Counting char occurance
     while ((c = getchar()) != EOF)
     {
         if (c > -1 && c < 128)
@@ -26,5 +53,11 @@ int main()
     for (int i = 0; i < 128; ++i)
     {
         printf("%c: %d\n", i, digits[i]);
+        if (digits[i] > max)
+        {
+            max = digits[i];
+        }
     }
+    print_histogram(digits, SIZE_OF_ASCII_TABLE, max);
+    printf("%d\n", max);
 }
